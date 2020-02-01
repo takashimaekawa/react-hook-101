c5870a5e01edb669f72293304446f810bebbbd69
github takashimaekawa <takashi.maekawa@outlook.com>
2020-02-01 15:36:37 +0900

    usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(previousCount => previousCount * 2)
+  const divid3 = () => setCount(previousCount =>{
+      if(previousCount % 3 === 0){
+         return previousCount / 3
+      }else{
+        return previousCount
+      }
+  })
+
   return (
-    <div>
-        text
-    </div>
+    <>
+        <div>count:{count}</div>
+        <div>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+        </div>
+        <div>
+            <button onClick={increment2}>+1</button>
+            <button onClick={decrement2}>-1</button>
+        </div>
+        <div>
+            <button onClick={reset}>reset</button>
+        </div>
+        <div>
+            <button onClick={double}>*2</button>
+        </div>
+
+        <div>
+            <button onClick={divid3}>%3</button>
+        </div>
+    </>
+    
   );
 }
 
