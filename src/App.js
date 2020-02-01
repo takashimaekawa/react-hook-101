d8f74cf7dba8b2e98edae52eb541fe03084217ec
github takashimaekawa <takashi.maekawa@outlook.com>
2020-02-01 18:27:33 +0900

    hatu

--- src/App.js
+++ src/App.js
@@ -2,49 +2,30 @@ import React, { useState } from 'react';
 
 
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = Props => {
 
+  const[name, setName] = useState(Props.name)
+  const[price, setPrice] = useState(Props.price)
 
-
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const reset = () => setCount(0)
-  const double = () => setCount(previousCount => previousCount * 2)
-  const divid3 = () => setCount(previousCount =>{
-      if(previousCount % 3 === 0){
-         return previousCount / 3
-      }else{
-        return previousCount
-      }
-  })
+  const reset = () => {
+      setPrice(Props.price)
+      setPrice(Props.name)
+  }
 
   return (
     <>
-        <div>count:{count}</div>
-        <div>
-            <button onClick={increment}>+1</button>
-            <button onClick={decrement}>-1</button>
-        </div>
-        <div>
-            <button onClick={increment2}>+1</button>
-            <button onClick={decrement2}>-1</button>
-        </div>
-        <div>
-            <button onClick={reset}>reset</button>
-        </div>
-        <div>
-            <button onClick={double}>*2</button>
-        </div>
-
-        <div>
-            <button onClick={divid3}>%3</button>
-        </div>
+        <p>現在の{name}は、{price}です。</p>
+        <button onClick={()=>setPrice(price + 1)}>+1</button>
+        <button onClick={()=>setPrice(price - 1)}>-1</button>
+        <button onClick={()=>reset()}>reset</button>
+        <input value = {name} onChange={e => setName(e.target.value)}/>
     </>
-    
   );
+}
+
+App.defaultProps = {
+    name:'',
+    price:1000
 }
 
 export default App;
